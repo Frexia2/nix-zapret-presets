a2c79b9c136ae05444e4af12cd8a86878143a154
kotudemo <130310308+kotudemo@users.noreply.github.com>
2025-06-02 19:27:44 +0400

    Add files via upload

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+        secret_files.url = "github:kotudemo/secret_files";
+    zapret-hostlists.url = "github:kotudemo/zapret-hostlists";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    secret_files,
+    zapret-hostlists,
+    ...
+  } @ inputs: let
+    forAllSystems = f:
+      nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ] (system: f (import nixpkgs {inherit system;}));
+  in {
+    formatter = forAllSystems (pkgs: pkgs.alejandra);
+    nixosModules.presets = import ./module.nix self;
+  };
+}
